49c1c2e086a8e6f21e89c6affbcbe1a9d8b3e93a
Sergey <vlasov.ft@gmail.com>
2018-04-07 21:05:22 +0500

    re #17 Simple Settings Fragment

--- app/src/main/java/com/yandex/android/androidapp/fragments/SettingsFragment.kt
+++ app/src/main/java/com/yandex/android/androidapp/fragments/SettingsFragment.kt
@@ -1,13 +1,18 @@
 package com.yandex.android.androidapp.fragments
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import com.yandex.android.androidapp.ContainerUI
 import com.yandex.android.androidapp.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SettingsFragment : Fragment() {
 
@@ -30,8 +35,59 @@ class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
+        val rootView = inflater?.inflate(R.layout.fragment_settings, container, false)!!
         containerUi?.setActivityTitle(R.string.settings_title)
-        return inflater?.inflate(R.layout.fragment_settings, container, false)!!
+
+        val sortBySettings = rootView.findViewById<ConstraintLayout>(R.id.sort_settings)
+        val sortByValueTextView = sortBySettings.findViewById<TextView>(R.id.sort_setting_config)
+        sortByValueTextView.text = resources.getString(R.string.by_edit_time_text)
+
+        sortBySettings.setOnClickListener {
+            showPopupMenu(it, R.menu.sortby_menu, sortByValueTextView) }
+
+        val sortOrderSettings = rootView.findViewById<ConstraintLayout>(R.id.sort_order)
+        val sortOrderValueTextView = sortOrderSettings.findViewById<TextView>(R.id.sort_order_value)
+        sortOrderValueTextView.text = resources.getString(R.string.descent_text)
+
+        sortOrderSettings.setOnClickListener {
+            showPopupMenu(it, R.menu.sort_order, sortOrderValueTextView) }
+
+        val simpleDateFormat = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+
+        val calendar = Calendar.getInstance()
+
+        val filterSettings = rootView.findViewById<ConstraintLayout>(R.id.filter_settings)
+        val filterValue = filterSettings.findViewById<TextView>(R.id.filter_by_value)
+
+
+
+        val onDateSetListener = DatePickerDialog.OnDateSetListener {
+            _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            calendar.set(year, month, dayOfMonth)
+            filterValue.text = simpleDateFormat.format(calendar.time)
+        }
+
+        val datePickerDialog = DatePickerDialog(context, onDateSetListener,
+                calendar[Calendar.YEAR], calendar[Calendar.MONTH], calendar[Calendar.DAY_OF_MONTH])
+
+
+        filterSettings.setOnClickListener {
+            datePickerDialog.show()
+        }
+
+
+        return rootView
     }
+
+    private fun showPopupMenu(view: View, menuId: Int, textView: TextView ) {
+        val popupMenu = PopupMenu(context, view)
+        popupMenu.inflate(menuId)
+
+        popupMenu.setOnMenuItemClickListener {
+            textView.text = it.title
+            true
+            }
+        popupMenu.show()
+    }
+
 }
